770588c37921cf4e25c32ca138e17a72239ade1b
Bruni12 <brunnetteboy@gmail.com>
2020-06-09 16:14:35 -0600

    building navigation and adding packages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import router from "./utils/router";
+import { createAppContainer } from "react-navigation";
+const AppContainer = createAppContainer(router);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <AppContainer />
     </View>
   );
 }
@@ -12,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
