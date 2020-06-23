e7d7da7e908d3128ae72cdd82f041d69c6438d82
Bruni12 <brunnetteboy@gmail.com>
2020-06-23 14:33:37 -0600

    Auth Screen

---
+++ screens/auth/AuthScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, TouchableOpacity} from "react-native";
+
+export default () => {
+    return (
+        <View>
+            <Text>Login</Text>
+
+            <TouchableOpacity>
+                <Text>Already have account?  Register</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
