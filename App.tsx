6173bc486bd7555b2451b57fa612d34abf822110
Bruni12 <brunnetteboy@gmail.com>
2020-07-09 12:38:27 -0600

    built out current use provider

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import router from "./utils/router";
 import { createAppContainer } from "react-navigation";
+import CurrentUserProvider from "./providers/CurrentUserProvider";
 const AppContainer = createAppContainer(router);
 
 export default function App() {
   return (
+    <CurrentUserProvider>
       <AppContainer />
+    </CurrentUserProvider>
   );
 }
 
