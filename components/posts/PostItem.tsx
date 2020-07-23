7d1a472f3db4e6b4f367df299c5ed50b5d609ce0
Bruni12 <brunnetteboy@gmail.com>
2020-07-23 00:35:16 -0600

    Base Styles for meme captions

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {View, Text, Dimensions} from "react-native";
 import AutoHeightImage from 'react-native-auto-height-image';
 
+import postItemStyles from "../../styles/stacks/posts/postItemStyles";
+
 interface IPostItemProps {
     post: {
       id: number;
@@ -13,13 +15,23 @@ interface IPostItemProps {
 export default (props: IPostItemProps) => {
     const {name, post_image_url} = props.post;
 
-  return (
-  <View>
+  const img = () => {
+    return (
+      // @ts-ignore
       <AutoHeightImage
-          width={Dimensions.get("window").width}
-          source={{uri: post_image_url}}
-        />
-    <Text>{name}</Text>
+        width={Dimensions.get("window").width}
+        source={{uri: post_image_url}}
+     />
+  ); 
+};
+
+return (
+  <View style={postItemStyles.itemWrapper}>
+    <View style={postItemStyles.imageWrapper}>{img()}</View>
+
+    <View style={[postItemStyles.contentWrapper, postItemStyles.postHeading]}>
+      <Text style={postItemStyles.nameText}>{name}</Text>
+    </View>
   </View>
-  ) 
-}
+ );
+};
