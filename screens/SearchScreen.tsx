8fe96f5b45b95717c6d322855008b304006ff245
Bruni12 <brunnetteboy@gmail.com>
2020-09-12 16:23:38 -0600

    Fixed bug in API SearchScreen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
-import { 
+import React, { useState } from "react";
+import {
   Text,
   View,
   TextInput,
   TouchableOpacity,
   ActivityIndicator,
- } from 'react-native';
+} from "react-native";
 import * as SecureStore from "expo-secure-store";
-import Container from '../components/layouts/Container';
+import Container from "../components/layouts/Container";
 import api from "../utils/api";
 
 interface ISearchScreenProps {
@@ -18,7 +18,7 @@ interface ISearchScreenProps {
 export default (props: ISearchScreenProps) => {
   const [query, setQuery] = useState("");
 
-  const handleSearch = async() => {
+  const handleSearch = async () => {
     const token = await SecureStore.getItemAsync("memipedia_secure_token");
 
     const params = {
@@ -26,40 +26,43 @@ export default (props: ISearchScreenProps) => {
     };
 
     const headers = {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     };
 
-    api.get("memipedia_queries", {
-      params,
-      headers,
-    }).then(response => {
-      console.log("response from query", response.data);
-    }).catch(error => {
-      console.log("Error running query", error);
-    });
+    api
+      .get("memipedia_queries", {
+        params,
+        headers,
+      })
+      .then((response) => {
+        console.log("response from query", response.data);
+      })
+      .catch((error) => {
+        console.log("Error running query", error);
+      });
   };
 
   const searchBar = (
     <View>
       <TextInput
-      value={query}
-      onChangeText={(val) => setQuery(val)}
-      placeholderTextColor="white"
-      placeholder="Search for a meme"
-      onSubmitEditing={handleSearch}
+        value={query}
+        onChangeText={(val) => setQuery(val)}
+        placeholderTextColor="white"
+        placeholder="Search for a meme"
+        onSubmitEditing={handleSearch}
       />
 
-      <TouchableOpacity style={{marginTop: 20}} onPress={handleSearch}>
-        <Text style={{color: "white"}}>Search</Text>
+      <TouchableOpacity style={{ marginTop: 20 }} onPress={handleSearch}>
+        <Text style={{ color: "white" }}>Search</Text>
       </TouchableOpacity>
     </View>
   );
 
   return (
-   <Container navigate={props.navigation.navigate}>
-    <Text>SearchScreen</Text>
-    {searchBar}
-   </Container>
+    <Container navigate={props.navigation.navigate}>
+      <Text>SearchScreen</Text>
+      {searchBar}
+    </Container>
   );
 };
 
